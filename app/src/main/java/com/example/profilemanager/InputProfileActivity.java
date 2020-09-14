567b4c78fb6480ec6cb1cc4a52901e23aaa7597f
mag1ini <alex.maglini@gmail.com>
2020-09-14 13:06:49 +0300

    activity_input-profile created only, edited  activity_main xaml, class

---
+++ app/src/main/java/com/example/profilemanager/InputProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.profilemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_profile);
+    }
+}
